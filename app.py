4ca019fc72850eb313b2da0129c4d35d41c33df7
Ad-White <>
2023-09-25 15:23:59 +0000

    Add more code comments to app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from werkzeug.utils import secure_filename
 if os.path.exists("env.py"):
     import env
 
-
+# application configuration settings
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
@@ -21,6 +21,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+# cloudinary configuration settings
 cloudinary.config(
     cloud_name=os.environ.get("CLOUD_NAME"),
     api_key=os.environ.get("API_KEY"),
@@ -32,16 +33,19 @@ cloudinary.config(
 @app.route("/show_vehicles")
 def show_vehicles():
     """
-    Returns and displays all vehicles from the database
+    Returns and displays all vehicles from the database, to
+    the show vehicles page.
     """
     vehicles = list(mongo.db.vehicles.find())
     return render_template("show_vehicles.html", vehicles=vehicles)
 
 
+# search functionality
 @app.route("/search", methods=["GET", "POST"])
 def search():
     """
-    Searches for matching resuluts from the query form
+    Searches for matching resuluts from the query input on
+    the show vehicles page.
     """
     query = request.form.get("query")
     vehicles = list(mongo.db.vehicles.find({"$text": {"$search": query}}))
@@ -129,10 +133,9 @@ def sign_out():
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     """
-    Returns and displays all vehicles from the database
-    for the user in session.
+    Returns and displays all vehicles for the user in session 
+    from the database. Displaying on the Profile page.
     """
-    # get the session user's username from the database
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
     if session["user"]:
@@ -145,8 +148,8 @@ def profile(username):
 @app.route("/add_new_vehicle", methods=["GET", "POST"])
 def add_new_vehicle():
     """
-    The user can add new vehicle information and the users
-    own image, using the Cloudinary API, to the database.
+    The user can add a new vehicle with information, and the users
+    own image (using the Cloudinary API), to the database.
     """
     if request.method == "POST":
         current_owner = "yes" if request.form.get("current_owner") else "no"
@@ -182,8 +185,9 @@ def add_new_vehicle():
 def edit_vehicle(vehicle_id):
     """
     The user can edit any vehicle information and the users
-    own image, using the Cloudinary API, and save changes
+    own image (using the Cloudinary API), and save changes
     to the database.
+    Returns the user to the show vehicles page.
     """
     if request.method == "POST":
         current_owner = "yes" if request.form.get("current_owner") else "no"
@@ -221,19 +225,32 @@ def edit_vehicle(vehicle_id):
 
 @app.route("/delete_vehicle/<vehicle_id>")
 def delete_vehicle(vehicle_id):
+    """
+    Upon user confirmation this deletes the vehicle from the database
+    with the assocciated vehicle id.
+    Notifies the user when action is complete.
+    """
     mongo.db.vehicles.delete_one({"_id": ObjectId(vehicle_id)})
     flash("Vehicle Successfully Deleted")
     return redirect(url_for("show_vehicles"))
 
 
+# manage vehicle types - Admin only
 @app.route("/get_vehicle_types")
 def get_vehicle_types():
+    """
+    Returns and displays all vehicle types from the database.
+    """
     vehicle_types = list(mongo.db.vehicle_types.find().sort("vehicle_make", 1))
     return render_template("vehicle_types.html", vehicle_types=vehicle_types)
 
 
 @app.route("/add_vehicle_type", methods=["GET", "POST"])
 def add_vehicle_type():
+    """
+    The Admin user can add a new vehicle type and
+    save this to the database.
+    """
     if request.method == "POST":
         vehicle_type = {
             "vehicle_type": request.form.get("vehicle_type")
@@ -247,6 +264,10 @@ def add_vehicle_type():
 
 @app.route("/edit_vehicle_type/<vehicle_type_id>", methods=["GET", "POST"])
 def edit_vehicle_type(vehicle_type_id):
+    """
+    The Admin user can edit a vehicle type and
+    save this to the database.
+    """
     if request.method == "POST":
         edit_type = {"$set": {
             "vehicle_type": request.form.get("vehicle_type")
@@ -263,11 +284,18 @@ def edit_vehicle_type(vehicle_type_id):
 
 @app.route("/delete_vehicle_type/<vehicle_type_id>")
 def delete_vehicle_type(vehicle_type_id):
+    """
+    Upon Admin confirmation to delete.
+    This deletes the vehicle type from the database with
+    the assocciated vehicle type id.
+    Notifies the user when action is complete.
+    """
     mongo.db.vehicle_types.delete_one({"_id": ObjectId(vehicle_type_id)})
     flash("Vehicle Type Deleted")
     return redirect(url_for("get_vehicle_types"))
 
 
+# Error handlers for 404 and 500 page responces
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
